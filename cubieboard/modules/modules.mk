60a79a38b5aa54a8f4e93fc4a65caa2b4e5f180e
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2013-03-18 05:00:51 +0100

    update kernel and modules (3.0.62)
    built from https://github.com/tpruvot/linux-sunxi (ics branch)

--- cubieboard/modules/modules.mk
+++ cubieboard/modules/modules.mk
@@ -1,4 +1,4 @@
-# modules.mk generate by extract-files.sh , do not edit it !!!!
+ifeq ($(TARGET_KERNEL_CONFIG),)
 PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/modules,system/lib/modules)
-
+endif
